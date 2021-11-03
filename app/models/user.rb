b7d98ee33bb7cf05ae19b3057ea1dd769abb952c
kmikiya <k.mikiya97@gmail.com>
2021-11-03 13:48:34 +0000

    フォロー機能完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,30 @@ class User < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments, dependent: :destroy
 	has_many :favorited_users, through: :favorites, source: :book
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+	has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+
   attachment :profile_image, destroy: false
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+
+  def follow(user_id)
+    relationship = relationships.new(followed_id: user_id)
+    relationship.save
+  end
+
+   def unfollow(user_id)
+    relationship = relationships.find_by(followed_id: user_id)
+    relationship.destroy
+   end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
