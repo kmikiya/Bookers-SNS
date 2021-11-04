b7a1767c9610375d59d84fa3c52e698d717a3970
kmikiya <k.mikiya97@gmail.com>
2021-11-04 04:26:23 +0000

    dm機能

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
     get 'followings' => "relationships#followings", as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
 end
